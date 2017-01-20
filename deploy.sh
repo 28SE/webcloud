44c58ee08cc162b72bcec773f6dac16ba116cda4
jun <sijun@hotmail.fr>
2017-01-20 13:49:14 +0100

    modification:notedecours et deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-tail -n +3 "$0" | ssh root@138.68.168.150 ; exit
-set -eun n
+#tail -n +3 "$0" | ssh root@138.68.168.150 ; exit
+#set -eu
+
+# mis a jour des la machine virtuelle
+apt-get update -y
+apt-get upgrade -y
+
+#installer nginx et git
+apt-get install nginx -y
+apt-get install git -y
+
+
+cd /var/www/html -y
+
+#récupérer le contenu de dépot git
+git clone https://github.com/28SE/webcloud.git
+
+
+cd /var/www/html/webcloud
+
+#copier le fichier index.html dans /var/www/html
+cp index.html /var/www/html/
+
+
